3a447321bea015a8f3a73499e68b82fb81f5381d
xvferdy <xv.ferdy@gmail.com>
2022-04-21 06:05:58 +0800

    ♻️ [refactor]: Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,63 +11,65 @@ function Footer() {
           <div className="address">
             <a className="address__logo" href="#">
               <Image
-                width={33}
-                height={33}
+                width={25}
+                height={25}
                 src="/assets/youtube.png"
                 alt="logo"
               />
               <h4 href="#">Visume</h4>
             </a>
-
-            <p>2238 Partha Elen Street, New Jersey, USA, 3322145698</p>
-            <div className="address__socials">
-              <Link href="#" passHref>
-                <a>
-                  <Image
-                    src="/assets/facebook.png"
-                    width={24}
-                    height={24}
-                    alt="Facebook"
-                  />
-                </a>
-              </Link>
-              <Link href="#" passHref>
-                <a>
-                  <Image
-                    src="/assets/twitter.png"
-                    width={24}
-                    height={24}
-                    alt="Twitter"
-                  />
-                </a>
-              </Link>
-              <Link href="#" passHref>
-                <a>
-                  <Image
-                    src="/assets/instagram.png"
-                    width={24}
-                    height={24}
-                    alt="Instagram"
-                  />
-                </a>
-              </Link>
-              <Link href="#" passHref>
-                <a>
-                  <Image
-                    src="/assets/linkedin.png"
-                    width={24}
-                    height={24}
-                    alt="Linkedin"
-                  />
-                </a>
-              </Link>
+            <div className="content">
+              <p>2238 Partha Elen Street, New Jersey, USA, 3322145698</p>
+              <div className="address__socials">
+                <Link href="#" passHref>
+                  <a>
+                    <Image
+                      src="/assets/facebook.png"
+                      width={24}
+                      height={24}
+                      alt="Facebook"
+                    />
+                  </a>
+                </Link>
+                <Link href="#" passHref>
+                  <a>
+                    <Image
+                      src="/assets/twitter.png"
+                      width={24}
+                      height={24}
+                      alt="Twitter"
+                    />
+                  </a>
+                </Link>
+                <Link href="#" passHref>
+                  <a>
+                    <Image
+                      src="/assets/instagram.png"
+                      width={24}
+                      height={24}
+                      alt="Instagram"
+                    />
+                  </a>
+                </Link>
+                <Link href="#" passHref>
+                  <a>
+                    <Image
+                      src="/assets/linkedin.png"
+                      width={24}
+                      height={24}
+                      alt="Linkedin"
+                    />
+                  </a>
+                </Link>
+              </div>
             </div>
           </div>
 
           {/* USEFULL LINKS */}
           <div className="usefull">
             <h4>Useful Links</h4>
-            <ul>
+
+            <ul className="content">
               <li>
                 <Link href="#" passHref>
                   <a>Payment & Tax</a>
@@ -94,7 +96,8 @@ function Footer() {
           {/*OUR COMPANY */}
           <div className="company">
             <h4>Our Company</h4>
-            <ul>
+
+            <ul className="content">
               <li>
                 <Link href="#" passHref>
                   <a>About us</a>
@@ -121,16 +124,19 @@ function Footer() {
           {/* NEWSLETTER */}
           <div className="newsletter">
             <h4>Subscribe to Newsletter</h4>
-            <form>
-              <input
-                name="subscribe"
-                type="email"
-                placeholder="Email Address"
-                required
-              />
-            </form>
-            <p>help@visume.link</p>
-            <p>396-987 456 885</p>
+
+            <div className="content">
+              <form>
+                <input
+                  name="subscribe"
+                  type="email"
+                  placeholder="Email Address"
+                  required
+                />
+              </form>
+              <p>help@visume.link</p>
+              <p>396-987 456 885</p>
+            </div>
           </div>
         </div>
 
